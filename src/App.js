c99d1e48f65b6aac362a3425eda5995ed062f242
Radgab <radgab712@gmail.com>
2024-03-11 23:14:25 +0300

    add

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import jsonData from './test.json';
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
